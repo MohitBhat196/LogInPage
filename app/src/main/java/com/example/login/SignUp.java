bdd074399c14cb5f47b1e06e3305cd139b514735
hellofrommohit <mohitbhat0119@gmail.com>
2022-02-01 21:30:29 +0530

    Second commit

--- app/src/main/java/com/example/login/SignUp.java
+++ app/src/main/java/com/example/login/SignUp.java
@@ -54,7 +54,7 @@ public class SignUp extends AppCompatActivity {
                 Pattern names = Pattern.compile("^[a-zA-Z]+$");
                 Pattern mobileNumber = Pattern.compile("^[0-9]{10}+$");
                 Pattern mailId = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+.+[a-zA-Z]+$");
-                Pattern passcode = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+                Pattern passcode = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$");
 
                 Matcher firstNameValueMatch = names.matcher(firstNameValue);
                 boolean firstNameValueMatches = firstNameValueMatch.matches();
@@ -72,67 +72,68 @@ public class SignUp extends AppCompatActivity {
                 Matcher password1ValueMatch = passcode.matcher(password1Value);
                 boolean password1ValueMatches = password1ValueMatch.matches();
 
-
-                if(!firstNameValueMatches){
-                    Toast.makeText(SignUp.this, "Please enter your First Name correctly", Toast.LENGTH_LONG).show();
-                }else if(!lastNameValueMatches){
-                    Toast.makeText(SignUp.this, "Please enter your Last Name correctly", Toast.LENGTH_LONG).show();
-                }else if(!email1ValueMatches){
-                    Toast.makeText(SignUp.this, "Please enter your Email Id correctly", Toast.LENGTH_LONG).show();
-
-                }else if(!phoneValueMatches){
-                    Toast.makeText(SignUp.this, "Please enter your Phone Number correctly(Use only digits!!)", Toast.LENGTH_LONG).show();
-                }else if(!password1ValueMatches){
-                    Toast.makeText(SignUp.this, "Use at least one lowercase, one uppercase, one special character and one number", Toast.LENGTH_LONG).show();
-
-                }else{
-
-                    if (firstNameValue.equals("") || lastNameValue.equals("") || email1Value.equals("") || password1Value.equals("") || phoneValue.equals("")) {
+                if (firstNameValue.equals("") || lastNameValue.equals("") || email1Value.equals("") || password1Value.equals("") || phoneValue.equals("")) {
                     Toast.makeText(SignUp.this, "FILL REQUIRED DETAILS", Toast.LENGTH_SHORT).show();
+                }
 
-                     }else{
-                        Boolean checkEmailId = db.checkEmail(email1Value);
-                        if(checkEmailId == false){
-                            boolean isInserted = db.Insert(email1Value, password1Value, firstNameValue, lastNameValue, phoneValue);
+                if (!firstNameValueMatches || !lastNameValueMatches || !email1ValueMatches || !phoneValueMatches || !password1ValueMatches) {
+                    if (!firstNameValueMatches) {
+                        Toast.makeText(SignUp.this, "Please enter your First Name correctly", Toast.LENGTH_LONG).show();
+                    } else if (!lastNameValueMatches) {
+                        Toast.makeText(SignUp.this, "Please enter your Last Name correctly", Toast.LENGTH_LONG).show();
+                    } else if (!email1ValueMatches) {
+                        Toast.makeText(SignUp.this, "Please enter your Email Id correctly", Toast.LENGTH_LONG).show();
+
+                    } else if (!phoneValueMatches) {
+                        Toast.makeText(SignUp.this, "Please enter your Phone Number correctly(Use only digits!!)", Toast.LENGTH_LONG).show();
+                    } else if (!password1ValueMatches) {
+                        Toast.makeText(SignUp.this, "Use at least one lowercase, one uppercase, one special character and one number", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+
+                    //if (firstNameValue.equals("") || lastNameValue.equals("") || email1Value.equals("") || password1Value.equals("") || phoneValue.equals("")) {
+                    //Toast.makeText(SignUp.this, "FILL REQUIRED DETAILS", Toast.LENGTH_SHORT).show();
+                    Boolean checkEmailId = db.checkEmail(email1Value);
+                    if (checkEmailId == false) {
+                        boolean isInserted = db.Insert(email1Value, password1Value, firstNameValue, lastNameValue, phoneValue);
                         if (isInserted == true) {
-                                Toast.makeText(SignUp.this, "REGISTERED", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUp.this, "REGISTERED", Toast.LENGTH_SHORT).show();
                         } else {
-                                Toast.makeText(SignUp.this, "NOT REGISTERED", Toast.LENGTH_SHORT).show();
-
+                            Toast.makeText(SignUp.this, "NOT REGISTERED", Toast.LENGTH_SHORT).show();
 
 
                         }
-                        }else{
+                    } else {
                         Toast.makeText(SignUp.this, "ALREADY REGISTERED", Toast.LENGTH_SHORT).show();
-                        }
+                    }
 
-                        }
-                        }
-            }});
+                }
+            }
+          });
 
-       // view.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-           // public void onClick(View view) {
-             //   Cursor cursor = db.getData();
-               // if(cursor.getCount() == 0){
-                 //   showMessage("Error", "Nothing Found");
-                   // return;}
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = db.getData();
+                if(cursor.getCount() == 0){
+                    showMessage("Error", "Nothing Found");
+                    return;}
 
-                    //StringBuffer buffer = new StringBuffer();
-                    //while(cursor.moveToNext()){
-                      //  buffer.append("ID : "+ cursor.getString(0)+"\n");
-                        //buffer.append("Email : "+ cursor.getString(1)+"\n");
-                        //buffer.append("Password : "+ cursor.getString(2)+"\n");
-                        //buffer.append("First Name : "+ cursor.getString(3)+"\n");
-                        //buffer.append("Last Name : "+ cursor.getString(4)+"\n");
-                        //buffer.append("Phone number : "+ cursor.getString(5)+"\n");
-                    //}
-                    //showMessage("REGISTERED DATA", buffer.toString());
-                //}
+                    StringBuffer buffer = new StringBuffer();
+                    while(cursor.moveToNext()){
+                        buffer.append("ID : "+ cursor.getString(0)+"\n");
+                        buffer.append("Email : "+ cursor.getString(1)+"\n");
+                        buffer.append("Password : "+ cursor.getString(2)+"\n");
+                        buffer.append("First Name : "+ cursor.getString(3)+"\n");
+                        buffer.append("Last Name : "+ cursor.getString(4)+"\n");
+                        buffer.append("Phone number : "+ cursor.getString(5)+"\n");
+                    }
+                    showMessage("REGISTERED DATA", buffer.toString());
+                }
 
 
 
-        //});
+        });
 
 
 
@@ -165,15 +166,15 @@ public class SignUp extends AppCompatActivity {
         phone.setText("");
     }
 
-    //public void showMessage(String title, String Message) {
-      //  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        //builder.setCancelable(true);
-        //builder.setTitle(title);
-        //builder.setMessage(Message);
-        //builder.show();
+    public void showMessage(String title, String Message) {
+      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
 
 
-    //}
+    }
 
 
 }
